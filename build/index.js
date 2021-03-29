51f9987035826a83de9aba0bb6d108daa5d8911f
kj-ninja <jakubiak.krzy@gmail.com>
2021-03-29 18:58:20 +0200

    npm was init also added nodemon and concurrently libs to the project

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('Yo!');
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([3, 2, 5, -1, 10, 8]);
+sorter.sort();
+console.log(sorter.collection);
