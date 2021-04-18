b72690746f02c10354f2e704fc15aa8a4e9d7871
kj-ninja <jakubiak.krzy@gmail.com>
2021-04-18 18:17:07 +0200

    added LinkedList class, removed sortable interface and changed Sorter class to abstract class now all child classes using inheritance and sorting method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import {Sorter} from "./Sorter";
 import {NumbersCollection} from "./NumbersCollection";
 import {CharactersCollection} from "./CharactersCollection";
+import {LinkedList} from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([2, 1, 5, 0, 4, 3, -1, -10]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([2, 1, 5, 0, 4, 3, -1, -10]);
+numbersCollection.sort();
+console.log(numbersCollection);
 
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(200);
+linkedList.add(100);
+linkedList.add(-30);
+linkedList.add(-50);
+
+linkedList.sort();
+linkedList.print()
